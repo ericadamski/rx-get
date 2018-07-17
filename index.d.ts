48f7fa730a5cfb414ef5b056956b404422afdcca
Eric Adamski <er.adamski@gmail.com>
2018-07-17 07:29:12 -0400

    Add typings

---
+++ index.d.ts
@@ -0,0 +1,40 @@
+// Type definitions for rx-get
+// Project: https://github.com/ericadamski/rx-get
+// Definitions by: Eric Adamski <ericadamski.github.io>
+
+import { Agent } from 'https';
+import { Observable } from 'rxjs';
+
+/// <reference types="node" />
+
+type RxGetResponse = {
+  status: number;
+  statusText: string;
+  headers: Object;
+  body: Buffer;
+  progress: number;
+  done: boolean;
+  json: () => JSON;
+  text: () => string;
+};
+
+export function get(
+  url: string,
+  options?: {
+    protocol?: string;
+    host?: string;
+    hostname?: string;
+    family?: number;
+    port?: number;
+    localAddress?: string;
+    sockectPath?: string;
+    method?: string;
+    path?: string;
+    headers?: Object;
+    auth?: string;
+    agent?: Agent | boolean;
+    createConnection?: Function;
+    timeout?: number;
+    body?: string;
+  }
+): Observable<RxGetResponse>;
